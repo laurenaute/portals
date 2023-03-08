0368bd5cf075c10360c21213adbfc6cf057c0179
Guillaume Laurent <laurenaute@gmail.com>
2023-03-08 10:36:55 +0100

    fix stimulus

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -4,7 +4,6 @@ import Typed from "typed.js"
 // Connects to data-controller="typed-js"
 export default class extends Controller {
   connect() {
-    console.log("je suis co")
     new Typed(this.element, {
       strings: ["Live your adventure", "A unique adventure"],
       typeSpeed: 50,
