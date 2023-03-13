3d6072b298ebd04b6c0d8c66e3e7efcd21ff9e19
jean lafore <jean.l94240@gmail.com>
2023-03-13 12:58:26 +0100

    loading page ok

---
+++ app/javascript/controllers/loading_page_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus"
+import Typed from "typed.js"
+
+// Connects to data-controller="loading-page"
+export default class extends Controller {
+  static targets = [ "loader", "loaderText" ]
+
+  connect() {
+  }
+
+  launchLoader() {
+    console.log("je charge");
+    this.loaderTarget.style.display = "initial";
+    new Typed(this.loaderTextTarget, {
+      strings: ["Your game is loading ..."],
+      typeSpeed: 90,
+      loop: true
+    });
+  };
+}
