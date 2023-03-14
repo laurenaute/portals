b7feb8eaaf8df6f03289ff58f4862becbaea316a
laurenaute <122482176+laurenaute@users.noreply.github.com>
2023-03-14 12:45:34 +0100

    Update adventure_form_controller.js

--- app/javascript/controllers/adventure_form_controller.js
+++ app/javascript/controllers/adventure_form_controller.js
@@ -2,7 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="adventure-form"
 export default class extends Controller {
-  static targets = [ "form" ]
 
   connect() {
   }
