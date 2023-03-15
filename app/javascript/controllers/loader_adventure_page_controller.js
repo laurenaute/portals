18b235dcc74b137ead2f8a0a7156dbc3e3f13b98
jean lafore <jean.l94240@gmail.com>
2023-03-15 18:06:03 +0100

    message disparait

--- app/javascript/controllers/loader_adventure_page_controller.js
+++ app/javascript/controllers/loader_adventure_page_controller.js
@@ -3,7 +3,7 @@ import Typed from "typed.js"
 
 // Connects to data-controller="loader-adventure-page"
 export default class extends Controller {
-  static targets = [ "button", "loader", "text" ]
+  static targets = [ "button", "loader", "text", "choiceButton" ]
   connect() {
     console.log("loadddddd");
     console.log(this.buttonTarget);
@@ -13,7 +13,8 @@ export default class extends Controller {
     console.log("click")
     this.loaderTarget.style.display = "block";
     this.loaderTarget.style.opacity = "1";
-    this.textTarget.style.display = "initial"
-    this.textTarget.style.opacity = "1"
+    this.textTarget.style.display = "initial";
+    this.textTarget.style.opacity = "1";
+    this.choiceButtonTarget.style.display = "none";
   }
 }
